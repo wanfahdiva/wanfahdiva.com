9e9dee3c541a441227bd98f060ed878cd62cc6a9
wanfahdiva <wanfah@sawala.tech>
2024-08-15 14:15:25 +0700

    chore: optimize landing project card layout and image loading

--- src/components/molecules/card/landing-project-card/landing-project-card.tsx
+++ src/components/molecules/card/landing-project-card/landing-project-card.tsx
@@ -55,31 +55,33 @@ export const LandingProjectCard = ({
 
   return (
     <div className="sticky top-0 flex items-center justify-center h-screen">
-      <motion.div
-        style={{ backgroundColor: getColor(index), scale, top: `calc(1vh + ${index * 25}px)` }}
-        className="flex flex-col relative md:h-[400px] w-11/12 mx-auto md:w-[850px] rounded-[25px] p-[50px] origin-top"
-      >
-        <Link href={url} target="_blank" passHref>
-          <h2
-            className="text-center m-0 text-2xl md:text-[28px] font-semibold text-onyx"
-            style={{ textShadow: '3px 2px 1px #ffffe3' }}
-          >
-            {title}
-          </h2>
+      <Link href={url} target="_blank" passHref>
+        <motion.div
+          style={{ backgroundColor: getColor(index), scale, top: `calc(1vh + ${index * 25}px)` }}
+          className="flex flex-col relative md:h-[400px] w-11/12 mx-auto md:w-[850px] rounded-[25px] p-[50px] origin-top"
+        >
+          <div>
+            <h2
+              className="text-center m-0 text-2xl md:text-[28px] font-semibold text-onyx"
+              style={{ textShadow: '3px 2px 1px #ffffe3' }}
+            >
+              {title}
+            </h2>
 
-          <div className="flex flex-col space-y-3 md:space-y-0 md:flex-row h-full mt-[50px] gap-[50px] text-[#1F2E48]">
-            <div className="md:w-3/5 relative top-[10%]">
-              <p className="text-[16px] first-letter:text-[28px] first-letter:font-['Title']">{description}</p>
-            </div>
+            <div className="flex flex-col space-y-3 md:space-y-0 md:flex-row h-full mt-[50px] gap-[50px] text-[#1F2E48]">
+              <div className="md:w-3/5 relative top-[10%]">
+                <p className="text-[16px] first-letter:text-[28px] first-letter:font-['Title']">{description}</p>
+              </div>
 
-            <div className="relative h-48 overflow-hidden rounded-lg shadow md:h-44 md:w-2/5">
-              <motion.div className="w-full h-full" style={{ scale: imageScale }}>
-                <NextImage layout="fill" src={src} alt={title} className="object-cover" useSkeleton />
-              </motion.div>
+              <div className="relative h-48 overflow-hidden rounded-lg shadow md:h-44 md:w-2/5">
+                <motion.div className="w-full h-full" style={{ scale: imageScale }}>
+                  <NextImage layout="fill" src={src} alt={title} className="object-cover" useSkeleton />
+                </motion.div>
+              </div>
             </div>
           </div>
-        </Link>
-      </motion.div>
+        </motion.div>
+      </Link>
     </div>
   )
 }
